3e25222737afd8b579ed531c19846231483c3c4d
Aditya Wasan <adityawasan55@gmail.com>
2019-08-20 23:48:44 +0530

    string length with issues
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ Recursion/CPP/Hackerrank/Recursion/string_find_length.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int length(char str[]) {
+    if (str[0] == '\0') {
+        return 0;
+    }
+    // cout<<str[0];
+    return 1 + length(str + 1);
+}
+
+int main() {
+    char str[] = {'1', '2', '3', '4'};
+    char str2[] = "sadsafas";   //IDK why its getting concatenated lol
+    cout<<length(str)<<endl;
+    // cout<<length(str2)<<endl;
+
+}
