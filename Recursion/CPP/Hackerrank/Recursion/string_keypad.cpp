5338a2086e31261c84f1a81c20118b2ab9e97c00
Aditya Wasan <adityawasan55@gmail.com>
2019-09-06 11:47:31 +0530

    Add shallow and deep copy
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- Recursion/CPP/Hackerrank/Recursion/string_keypad.cpp
+++ Recursion/CPP/Hackerrank/Recursion/string_keypad.cpp
@@ -2,26 +2,33 @@
 using namespace std;
 
 int keypadSeqs(int input, string output[], string ref[]) {
-    if (input == 0) {
-        output[0] = "";
-        return 1;
+    if (input <= 9) {
+        for (int i = 0; i < ref[input].size(); i++) {
+            output[i] = ref[input][i];
+        }
+        return ref[input].size();
     }
     int x = input % 10;
     int size = keypadSeqs(input/10, output, ref);
-    int k = size;
+    int k = 0;
+    string copyarray[size];
+
+    for (int i = 0; i < size; i++) {
+        copyarray[i] = output[i];
+    }
     for (int i = 0; i < ref[x].size(); i++) {
         for (int j = 0; j < size; j++) {
-            output[k++] = ref[x][i] + output[j];
+            output[k++] = copyarray[j] + ref[x][i];
         }
     }
-    return ref[x].size() * size + size;
+    return k;
 }
 
 int main() {
     
     string ref[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     string* output = new string[1000];
-    int x = keypadSeqs(22, output, ref);
+    int x = keypadSeqs(234, output, ref);
     for (int i = 0; i < x; i++) {
         cout << output[i] << endl;
     }
