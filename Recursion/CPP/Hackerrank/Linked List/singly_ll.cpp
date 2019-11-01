22a547aa1c8a255ebebaff0c2efb9d19b3e9bfa4
Aditya Wasan <adityawasan55@gmail.com>
2019-11-01 22:17:56 +0530

    implement more linked list functionality
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- Recursion/CPP/Hackerrank/Linked List/singly_ll.cpp
+++ Recursion/CPP/Hackerrank/Linked List/singly_ll.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 int main() {
     SingleLLHelper helper;
-    helper.addNode(10);
-    helper.addNode(20);
-    helper.addNode(30);
-    helper.addNode(40);
+    helper.addNodeAtPosition(0, 0);
+    helper.addNodeEnd(10);
+    helper.addNodeEnd(20);
+    helper.addNodeEnd(30);
+    helper.addNodeEnd(40);
     helper.printLL();
     helper.removeNode();
+    helper.printLL();
+    helper.addNodeAtPosition(4, 40);
+    helper.addNodeAtPosition(4, 35);
+    helper.printLL();
+    cout << helper.addNodeAtPosition(10, 35) << endl;
+    helper.addNodeAtPosition(0, -1);
     helper.printLL();
 
 }
