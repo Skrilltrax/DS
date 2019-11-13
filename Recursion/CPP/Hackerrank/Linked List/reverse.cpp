3cb9f2a724dbe20f0dd13c5da5e60d81749e35b4
Aditya Wasan <adityawasan55@gmail.com>
2019-11-13 14:28:32 +0530

    Add better methods to reverse a linked list
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- Recursion/CPP/Hackerrank/Linked List/reverse.cpp
+++ Recursion/CPP/Hackerrank/Linked List/reverse.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 typedef SingleLLHelper::Node Node;
 
+class Pair {
+    public:
+    Node* head;
+    Node* tail;
+};
+
+// Complexity O(n^2)
 Node* reverseRecursive(Node* head) {
     if (head -> next == NULL) {
         return head;
@@ -17,6 +24,32 @@ Node* reverseRecursive(Node* head) {
     return smallAns;
 }
 
+// Complexity O(n)
+Pair reverseRecursivePair(Node* head) {
+    if (head -> next == NULL) {
+        Pair ans;
+        ans.head = head;
+        ans.tail = head;
+        return ans;
+    }
+    Pair smallAns = reverseRecursivePair(head -> next);
+    head -> next = NULL;
+    smallAns.tail -> next = head;
+    smallAns.tail = smallAns.tail -> next;
+    return smallAns;
+}
+
+// Complexity O(n)
+Node* reverseRecursiveSimple(Node* head) {
+    if (head -> next == NULL) {
+        return head;
+    }
+    Node* smallAns = reverseRecursiveSimple(head -> next);
+    head -> next -> next = head;
+    head -> next = NULL;
+    return smallAns;
+}
+
 int main() {
     SingleLLHelper helper;
     helper.addNode(1);
@@ -24,6 +57,8 @@ int main() {
     helper.addNode(3);
     helper.addNode(4);
     helper.addNode(5);
-    helper.head = reverseRecursive(helper.head);
+    // helper.head = reverseRecursive(helper.head);
+    helper.head = reverseRecursivePair(helper.head).head;
+    // helper.head = reverseRecursiveSimple(helper.head);    
     helper.printLL();
 }
