0841d9549a2649dea65f03c40174965421c050d4
Aditya Wasan <adityawasan55@gmail.com>
2019-11-27 22:50:04 +0530

    Add script and improve queue
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- Recursion/CPP/Hackerrank/Stacks Queues/QueueArr.cpp
+++ Recursion/CPP/Hackerrank/Stacks Queues/QueueArr.cpp
@@ -4,6 +4,7 @@ using namespace std;
 template <typename T>
 class Queue {
     int size;
+    int capacity;
     int front;
     int rear;
     const int MAX_SIZE;
@@ -11,20 +12,34 @@ class Queue {
 
     public:
 
-    Queue(int size) : MAX_SIZE(100), front(-1), rear(-1) {
+    Queue(int size) : MAX_SIZE(100), front(-1), rear(-1), size(0) {
         if (size > MAX_SIZE) {
             cout << "QUEUE CANNOT BE GREATER THAN " << MAX_SIZE << endl;
             cout << "QUEUE SIZE IS SET TO " << MAX_SIZE << endl;
-            this -> size = MAX_SIZE;
+            this -> capacity = MAX_SIZE;
         } else {
-            this -> size = size;
+            this -> capacity = size;
         }
-        T data[this -> size];
+        T data[this -> capacity];
         cout << "QUEUE CREATED" << endl;
     }
 
+    T getFront() {
+        if (front != -1)
+            return data[front];
+        else
+            return INT8_MIN;
+    }
+
+    T getRear() {
+        if (rear != -1)
+            return data[rear];
+        else
+            return INT8_MIN;
+    }
+
     int getSize() {
-        return rear + 1;
+        return size;
     }
 
     bool isEmpty() {
@@ -32,25 +47,29 @@ class Queue {
     }
 
     void enqueue(T element) {
-        if (rear == size - 1) {
+        if (((rear + 1) % capacity) == front) {
             cout << "QUEUE FULL";
             return;
         }
         if (front == -1) {
             front++;
         }
-        data[++rear] = element;
+        rear = (rear + 1) % capacity;
+        data[rear] = element;
+        size++;
     }
 
     void dequeue() {
-        if (rear == -1) {
+        if (front == -1) {
             cout << "QUEUE EMPTY";
             return;
         }
         if (front == rear) {
+            size--;
             front = rear = -1;
         } else {
-            front++;
+            size--;
+            front = (front + 1) % capacity;
         }         
     }
 };
