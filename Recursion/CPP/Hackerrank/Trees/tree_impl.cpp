0bca7b98f8f841a8209f2b4e840c65213b6bce59
Aditya Wasan <adityawasan55@gmail.com>
2019-12-09 23:40:19 +0530

    Add trees
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ Recursion/CPP/Hackerrank/Trees/tree_impl.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "TreeNode.hpp"
+using namespace std;
+
+void printTree(TreeNode<int> *root) {
+    if (root == NULL) {
+        return;
+    }
+    cout << root -> data << " : ";
+    for (int i = 0; i < root -> children.size(); i++) {
+        cout << root -> children[i] -> data;
+        if (i != root -> children.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < root -> children.size(); i++) {
+        printTree(root -> children[i]);
+    }
+}
+
+int main () {
+
+    TreeNode<int> *root = new TreeNode<int>(10);
+    root -> children.push_back(new TreeNode<int>(20));
+    root -> children.push_back(new TreeNode<int>(30));
+    root -> children.push_back(new TreeNode<int>(40));
+    printTree(root);
+}
