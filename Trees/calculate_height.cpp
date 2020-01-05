c5668607a7038798d20526a52237b21247c628e5
Aditya Wasan <adityawasan55@gmail.com>
2020-01-05 23:54:07 +0530

    Moar and moar small progs
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ Trees/calculate_height.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "Tree.hpp"
+
+int calculateHeight(TreeNode<int>* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    vector<int> heights;
+    int max = INT_MIN;
+    for (int i = 0; i < root -> children.size(); i++) {
+        heights.push_back(calculateHeight(root -> children[i]));
+        if (max < heights[i]) {
+            max = heights[i];
+        }
+    }
+
+    if (max == INT_MIN) {
+        return 1;
+    }
+    return 1 + max;
+}
+
+int main() {
+    Tree<int> treeHelper;
+    treeHelper.createTreeBF();
+    TreeNode<int>* tree = treeHelper.getTree();
+    cout << "Height : " << calculateHeight(tree);
+    return 0;
+}
--- Trees/count_leaves.cpp
+++ Trees/count_leaves.cpp
@@ -5,7 +5,10 @@ int countNodes(TreeNode<int>* root) {
     if (root == NULL) {
         return 0;
     }
-    int sum = 1;
+    int sum = 0;
+    if (root -> children.size() == 0) {
+        sum = 1;
+    }
     for (int i = 0; i < root -> children.size(); i++) {
         sum += countNodes(root -> children[i]);
     }
@@ -16,6 +19,6 @@ int main() {
     Tree<int> treeHelper;
     treeHelper.createTreeBF();
     TreeNode<int>* tree = treeHelper.getTree();
-    cout << "No of Nodes : " << countNodes(tree);
+    cout << "No of Leaves : " << countNodes(tree);
     return 0;
 }
